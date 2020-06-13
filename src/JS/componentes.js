98d9a19c601a5bbf19e92c3a6ccc1d449321f52d
Diego Navarro <luis_d_100@hotmail.com>
2020-06-12 22:51:36 -0600

    configuracion para prod y dev. Hash para evitar cache

--- src/JS/componentes.js
+++ src/JS/componentes.js
@@ -6,7 +6,7 @@ export const saludar = (nombre) => {
 
     const h1 = document.createElement('h1');
 
-    h1.innerHTML = ` Saludar, ${nombre}`;
+    h1.innerHTML = `WebPack, ${nombre}`;
 
     document.body.append( h1);
     
--- webpack.config.js
+++ webpack.config.js
@@ -1,55 +1,71 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-
-const miniCSSstract = require ('mini-css-extract-plugin');
+const miniCSSstract = require('mini-css-extract-plugin');
 const OptimizeCssAssetsPluging = require('optimize-css-assets-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  
 
     mode: 'development',
-    optimization:{
+    optimization: {
         minimizer: [new OptimizeCssAssetsPluging()]
     },
     module: {
         rules: [
-        {   
-            test: /\.css$/,
-            exclude: /globalStyle\.css$/,
-            use: [
-                'style-loader',
-                'css-loader'
-            ]
-        },
-        {
-            test: /globalStyle\.css$/,
-            use: [
-                miniCSSstract.loader,
-                'css-loader'
-            ]
-        },
-           {
-               test: /\.html$/i,
-               loader: 'html-loader',
-               options: {
-                   attributes: true,
-                   minimize: false
-                   
-               }
-               
-           } 
+            {
+                test: /\.css$/,
+                exclude: /globalStyle\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /globalStyle\.css$/,
+                use: [
+                    miniCSSstract.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options: {
+                    attributes: false,
+                    minimize: false
+
+                }
+
+            },
+            {
+                test: /\.(png|svg|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false
+                        }
+                    },
+                ],
+            },
         ]
-    }, 
+    },
     plugins: [
         new HtmlWebPackPlugin({
-         template: './src/index.html',
-         filename: './index.html'  
+            template: './src/index.html',
+            filename: './index.html'
         }),
         new miniCSSstract({
             filename: '[name].css',
             ignoreOrder: false
-        })
+        }),
+
+        new CopyPlugin({
+            patterns: [
+                { from: 'src/assets', to: 'assets/' }
+            ],
+        }),
 
     ]
 
 
-  };
+};
