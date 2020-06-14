ed533824449f84d894d045050c1104237c19ab9b
Diego Navarro <luis_d_100@hotmail.com>
2020-06-13 23:11:30 -0600

    babel + dist clean

---
+++ webpack.prod.js
@@ -0,0 +1,85 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const miniCSSstract = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPluging = require('optimize-css-assets-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MinifyPlugin = require("babel-minify-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+
+    mode: 'production',
+    optimization: {
+        minimizer: [new OptimizeCssAssetsPluging()]
+    },
+
+    output:{
+        filename: 'main.[contentHash].js',
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                exclude: /globalStyle\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /globalStyle\.css$/,
+                use: [
+                    miniCSSstract.loader,
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+                options: {
+                    attributes: false,
+                    minimize: false
+
+                }
+
+            },
+            {
+                test: /\.(png|svg|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            esModule: false
+                        }
+                    },
+                ],
+            },
+            { 
+                test: /\.js$/, 
+                exclude: /node_modules/, 
+                loader: "babel-loader" 
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        }),
+        new miniCSSstract({
+            filename: '[name].[contentHash].css',
+            ignoreOrder: false
+        }),
+
+        new CopyPlugin({
+            patterns: [
+                { from: 'src/assets', to: 'assets/' }
+            ],
+        }),
+        new MinifyPlugin(),
+        
+        new CleanWebpackPlugin(),
+    ]
+
+
+};
