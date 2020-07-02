f59e0261f887bf88df5033481315c5614e36ffcd
Diego Navarro <luis_d_100@hotmail.com>
2020-07-01 23:16:06 -0600

    classes exportadas

--- src/index.js
+++ src/index.js
@@ -1,7 +1,21 @@
 import './CSS/styles.css';
 
-import {Todo} from './classes/todo.class';
+// import {Todo} from './classes/todo.class';
+// import { TodoList } from './classes/todo-list.class';
 
-const tarea = new Todo('Aprender Javascript');
+import { Todo, TodoList } from './classes';
 
+const todoList = new TodoList();
+
+const tarea = new Todo('Aprender Javascript')
+
+
+todoList.nuevoTodo( tarea );
+
+console.log(todoList);
 console.log(tarea);
+
+
+// todoList.nuevoTodo(tarea);
+
+// console.log(TodoList);
